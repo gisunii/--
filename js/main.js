e2f4e5091b3ae7ae569d2e7d5877ad6fd47e2e10
黄俐 <849151701@QQ.COM>
2017-09-21 22:40:50 +0800

    map优化
    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    var isMapAdded = false;
     // 导航按钮
     $('.btn-triger').click(function () {
 	    $(this).closest('.btn-group').toggleClass('open');
@@ -18,6 +19,9 @@ $(function () {
         });
         var dataNav = $(this).attr('data-nav');
         var pageItem = $('.data-nav-' + dataNav);
+        if(dataNav == 4 && !isMapAdded) {
+            isMapAdded = true;
+        }
         pageItem.animate({left:'0'});
         pageItem.addClass('active');
     });
@@ -34,31 +38,15 @@ $(function () {
     });
 
     // 地图页面
-    var positionArr = [[108.393135, 31.160711], [116.286407, 39.748232], [114.035504, 22.524612]]; // 开州,北京建筑大学,福田
-    var titileArr = ['重庆市开州区', '北京建筑大学', '深圳市福田区'];
-    var iconArr = ['pos-home.png', 'pos-school.png', 'pos-me.png'];
-    var map = new AMap.Map('map',{
-        zoom: 3,
-        center: positionArr[0],
-        mapStyle: 'amap://styles/graffiti'
-    });
-
-    $('#home').click(function(){
-        map.setZoomAndCenter(3, positionArr[0]);
-    });
-    
-    for (var i = 0; i < 3; i++) {
-        var icon = new AMap.Icon({
-            image: './images/' + iconArr[i],
-            size: new AMap.Size(24, 24)
+        var positionArr = [[108.393135, 31.160711], [116.286407, 39.748232], [114.035504, 22.524612]]; // 开州,北京建筑大学,福田
+        var titileArr = ['重庆市开州区', '北京建筑大学', '深圳市福田区'];
+        var iconArr = ['pos-home.png', 'pos-school.png', 'pos-me.png'];
+        var map = new AMap.Map('map',{
+            zoom: 3,
+            center: positionArr[0],
+            doubleClickZoom: false,
+            mapStyle: 'amap://styles/graffiti'
         });
-        marker = new AMap.Marker({
-            icon: icon,
-            position: positionArr[i],
-            zIndex: 101,
-            offset : new AMap.Pixel(-12,-12),
-            title: titileArr[i],
-            map: map
         });
         marker.on('click', function(e) {
             map.setZoomAndCenter(14, [e.target.getPosition().lng, e.target.getPosition().lat]);
@@ -80,5 +68,39 @@ $(function () {
         driving2.search(positionArr[0], positionArr[2], function(status, result) { });
         driving3.search(positionArr[1], positionArr[2], function(status, result) { });
     })
+        
+        for (var i = 0; i < 3; i++) {
+            var icon = new AMap.Icon({
+                image: './images/' + iconArr[i],
+                size: new AMap.Size(24, 24)
+            });
+            marker = new AMap.Marker({
+                icon: icon,
+                position: positionArr[i],
+                zIndex: 101,
+                offset : new AMap.Pixel(-12,-12),
+                title: titileArr[i],
+                map: map
+            });
+            marker.on('click', function(e) {
+                map.setZoomAndCenter(14, [e.target.getPosition().lng, e.target.getPosition().lat]);
+            });
+        }
+        
+        AMap.service('AMap.Driving',function() {
+            var drivingOption = {
+                map: map,
+                policy: AMap.DrivingPolicy.LEAST_DISTANCE,
+                hideMarkers: true,
+                autoFitView: false
+            };
+            var driving1 = new AMap.Driving(drivingOption);
+            var driving2 = new AMap.Driving(drivingOption);
+            var driving3 = new AMap.Driving(drivingOption);
     
+            driving1.search(positionArr[0], positionArr[1], function(status, result) { });
+            driving2.search(positionArr[0], positionArr[2], function(status, result) { });
+            driving3.search(positionArr[1], positionArr[2], function(status, result) { });
+        })
+    };
 })
